677bc91e3e05c3f3f74b084473f40e3bbb9a33ca
Dani <daniela.amorimdesa@gmail.com>
2025-10-29 14:48:53 -0300

    Improve comments for clarity in RobotNewsStationNode implementation

--- my_py_pkg/my_py_pkg/robot_news_station.py
+++ my_py_pkg/my_py_pkg/robot_news_station.py
@@ -9,13 +9,13 @@ class RobotNewsStationNode(Node):
         super().__init__("robot_news_station") # criar um nó chamado "robot_news_station"
         self.robot_name = "C3PO"
         self.publisher_ = self.create_publisher(String, "robot_news", 10) # criar um publicador no tópico "robot_news" com fila de tamanho 10
-        self.timer_ = self.create_timer(0.5, self.publish_news)
-        self.get_logger().info("Robot News has been started!")
+        self.timer_ = self.create_timer(0.5, self.publish_news) # criar um timer que chama publish_news a cada 0.5 segundos
+        self.get_logger().info("Robot News has been started!")  # registrar uma mensagem de log
 
     def publish_news(self):
-        msg = String()
+        msg = String() # criar uma mensagem do tipo String
         msg.data = f"Hello, this is {self.robot_name} from the robot news station :)"
-        self.publisher_.publish(msg)
+        self.publisher_.publish(msg) # publicar a mensagem
 
 
 def main(args=None):
