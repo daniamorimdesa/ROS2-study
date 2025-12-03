ccc7525ba5b6880c20bb8195a0fa50013624b11a
Dani <daniela.amorimdesa@gmail.com>
2025-12-03 14:18:10 -0300

    Implement MoveRobot action client and server with initial position handling

--- actions_py/actions_py/move_robot_client.py
+++ actions_py/actions_py/move_robot_client.py
@@ -1,17 +1,23 @@
 #!/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
+from rclpy.action import ActionClient
 from my_robot_interfaces.action import MoveRobot
 
 class MoveRobotClientNode(Node):
 
     def __init__(self):
         super().__init__("move_robot_client") # criar um nó chamado "move_robot_client"
+        self.move_robot_client_ = ActionClient(self, MoveRobot, "move_robot")
+
+    def send_goal(self, position, velocity):
+        pass
 
 
 def main(args=None):
     rclpy.init(args=args) # inicializar o rclpy
     node = MoveRobotClientNode() # instanciar o nó
+    node.send_goal(76,7)
     rclpy.spin(node) # manter o nó ativo para processar callbacks(até que se aperte Ctrl+C)
     rclpy.shutdown() # finalizar o rclpy
 #-----------------------------------------------------------------------------------------------
--- actions_py/actions_py/move_robot_server.py
+++ actions_py/actions_py/move_robot_server.py
@@ -10,51 +10,51 @@ class MoveRobotServerNode(Node):
 
     def __init__(self):
         super().__init__("move_robot_server") # criar um nó chamado "move_robot_server"
-        self.robot_position_ = 50
+        self.robot_position_ = 50             # posição inicial do robô
         self.move_robot_server_ = ActionServer(
             self,
             MoveRobot,
             "move_robot",
             execute_callback= self.execute_callback
         )
-        self.get_logger().info("Action Server has been started!")
-        self.get_logger().info(f"Robot position: {self.robot_position_}")
+        self.get_logger().info("Action Server has been started!")         # registrar uma mensagem de log
+        self.get_logger().info(f"Robot position: {self.robot_position_}") # registrar a posição inicial do robô
 
-    
-    def execute_callback(self, goal_handle: ServerGoalHandle):
-        goal_position = goal_handle.request.position
-        velocity = goal_handle.request.velocity
+    # executar a ação para mover o robô
+    def execute_callback(self, goal_handle: ServerGoalHandle): 
+        goal_position = goal_handle.request.position  # posição alvo do robô
+        velocity = goal_handle.request.velocity       # velocidade de movimento do robô
 
-        result = MoveRobot.Result()
-        feedback = MoveRobot.Feedback()
+        result = MoveRobot.Result()                   # criar um objeto para armazenar o resultado da ação
+        feedback = MoveRobot.Feedback()               # criar um objeto para armazenar o feedback da ação
 
         self.get_logger().info("Execute goal")
-        while rclpy.ok():
-            diff = goal_position - self.robot_position_
+        while rclpy.ok(): 
+            diff = goal_position - self.robot_position_ # diferença entre a posição alvo e a posição atual
 
-            if diff == 0:
+            if diff == 0: # posição alvo alcançada
                 result.position = self.robot_position_
                 result.message = "Sucess"
                 goal_handle.succeed()
                 return result
 
-            elif diff > 0:
+            elif diff > 0: # mover para a direita
                 if diff >= velocity: 
                     self.robot_position_ += velocity
                 else:
                     self.robot_position_ += diff
 
-            else:
+            else: # mover para a esquerda
                 if abs(diff) >= velocity:
                     self.robot_position_ -= velocity
                 else:
                     self.robot_position_ -= abs(diff)
 
             self.get_logger().info(f"Robot position: {self.robot_position_}")
-            feedback.current_position = self.robot_position_
-            goal_handle.publish_feedback(feedback)
+            feedback.current_position = self.robot_position_ # atualizar a posição atual no feedback
+            goal_handle.publish_feedback(feedback)           # publicar o feedback
 
-            time.sleep(1.0)
+            time.sleep(1.0)                                  # simular o tempo de movimento do robô
 
 
 def main(args=None):
